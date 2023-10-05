c6e31bf4b33bff43f34391d3edb2f5cb4b58e024
alexbrownx <alexbrownx@googlemail.com>
2023-10-05 01:11:37 +0100

    Optimises imports

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 using GameSetup;
 using UnityEngine;
 
--- Assets/Scripts/GameSetup/EnemyBoardSetup.cs
+++ Assets/Scripts/GameSetup/EnemyBoardSetup.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace GameSetup {
     public class EnemyBoardSetup : MonoBehaviour
